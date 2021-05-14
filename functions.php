2ec735eaf83689168803d72b75374931976c45aa
saperez17 <saperez@unicauca.edu.co>
2021-05-13 21:01:24 -0500

    fixed

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 function holamundo_shortcode( $atts , $content = null, $tag) {
-return '<h1>'.ucfirst(substr($tag,0,3)).ucfirst(substr($tag,3,-1)).'</h1>';
+return '<h1>'.ucfirst(substr($tag,0,3)).ucfirst(substr($tag,4,-1)).'</h1>';
 }
 
 //attach it to the wordpress system
